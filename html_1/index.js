3e30447dbbb322c4a7a14d7d5c062c140b5adc03
aqueelahi <148911857+aqueelahi@users.noreply.github.com>
2024-04-23 18:18:39 -0400

    Add files via upload

---
+++ html_1/index.js
@@ -0,0 +1,11 @@
+console.log ("Hello world!")
+alert ("Hello")
+
+const hairDescButton = document.getElementById("hairDescButton");
+const hairDescSelect = document.getElementById("hairdesc");
+
+hairDescButton.addEventListener("click",function () {
+    const hairDesc = hairDescSelect.value;
+    const resourcehtml = "coilyhaircare.html";
+    window.location.href= coilyhaircare.html
+});

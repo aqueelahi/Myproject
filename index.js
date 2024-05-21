b7bad24319c332f79e7c72b9f27e93404499d596
aqueelahi <aqueelahirshad2@gmail.com>
2024-05-21 16:16:38 -0400

    test

---
+++ index.js
@@ -0,0 +1,42 @@
+<<<<<<< HEAD
+=======
+console.log ("Hello world!")
+alert ("Hello")
+>>>>>>> cc61ad8 ( work on css)
+
+const hairDescButton = document.getElementById("hairDescButton");
+const hairDescSelect = document.getElementById("hairdesc");
+
+hairDescButton.addEventListener("click",function () {
+    const hairDesc = hairDescSelect.value;
+    const resourcehtml = "coilyhaircare.html";
+    window.location.href= coilyhaircare.html
+<<<<<<< HEAD
+});
+
+const hairDescButton = document.getElementById("hairDescButton");
+const hairDescSelect = document.getElementById("hairdesc");
+
+hairDescButton.addEventListener("click",function () {
+    const hairDesc = hairDescSelect.value;
+    const resourcehtml = "coilyhaircare.html";
+    window.location.href= coilyhaircare.html
+});
+
+const slidesContainer = document.getElementById("slides-container");
+const slide = document.querySelector(".slide");
+const prevButton = document.getElementById("slide-arrow-prev");
+const nextButton = document.getElementById("slide-arrow-next");
+
+nextButton.addEventListener("click", () => {
+  const slideWidth = slide.clientWidth;
+  slidesContainer.scrollLeft += slideWidth;
+});
+
+prevButton.addEventListener("click", () => {
+  const slideWidth = slide.clientWidth;
+  slidesContainer.scrollLeft -= slideWidth;
+});
+=======
+});
+>>>>>>> cc61ad8 ( work on css)
